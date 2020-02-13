723aac775fe227fef6b89c38bdbb90926bb7590d
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-13 00:24:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,43 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [12, 18, 27, 33]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["Snake","Cat","Dog"] << "Pig"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  ["Snake","Cat","Dog"].pop("Pig")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  ["Snake","Cat","Dog","Pig"].shift
 end
 
 def remove_element_from_start_of_array(array)
-  
+  ["Greg","Kenji","Tricia","Liz"].unshift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    ["Greg","Kenji","Tricia","Liz"]
+    pust [3]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  ["Greg","Kenji","Tricia","Liz"]
+  pust [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  ["Greg","Kenji","Tricia","Liz"]
+  pust [-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  ["Greg","Kenji","Tricia","Liz"]
+  [3] = ("Matt")
 end
