51754346d300477e2f9a6ef5e2a0f9b1807dc0b4
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-13 00:32:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   cute_pets = ["Snake","Cat","Dog"]
-  cute_pets << "Pig"
+  cute_pets << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
