13c220e95cfc8744bfb355d81d6e8f2b2a6194a7
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-13 01:00:47 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,41 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [12, 18, 27, 33]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  #cute_pets = ["Snake","Cat","Dog"]
+  #element = "Pigs"
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(names = ["Greg","Kenji","am","Liz"], index_number)
+  names[2]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(names = ["wow","Kenji","am","Greg"], index)
+  names[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(names = ["Greg","Kenji","Tricia","Liz"])
+  names[-1]
 end
 
-def update_element_from_index(array, index_number, element)
-
+def update_element_from_index(names = ["Greg","Kenji","Tricia","Liz"], index, element)
+  names[3] = "totally"
 end
