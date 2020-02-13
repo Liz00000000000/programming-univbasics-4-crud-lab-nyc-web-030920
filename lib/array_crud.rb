ab05ba0a7c41a164c27e5a7c635062482e829953
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-13 00:35:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ def add_element_to_start_of_array(cute_pets = ["Snake","Cat","Dog"], element)
 end
 
 def remove_element_from_end_of_array(array)
-  ["Snake","Cat","Dog","Pig"].shift
+  ["Snake","Cat","Dog","arrays!"].shift
 end
 
 def remove_element_from_start_of_array(array)
