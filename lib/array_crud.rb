1cf097c29b1f32f8fe6b4636f9cae5ee05935f44
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-13 00:30:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   cute_pets = ["Snake","Cat","Dog"]
-  cute_pets.pop("Pig!")
+  cute_pets << "Pig"
 end
 
 def remove_element_from_end_of_array(array)
